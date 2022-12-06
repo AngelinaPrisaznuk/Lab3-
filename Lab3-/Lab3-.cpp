de8a90ff0980612fd8a43af0aa8e6fa3c0a52d5b
AngelinaPrisaznuk <114239162+AngelinaPrisaznuk@users.noreply.github.com>
2022-12-06 19:50:50 +0200

    Add project files.

---
+++ Lab3-/Lab3-.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    //Операція ділення
+    int a, b;
+    int c;
+    float x;
+    a = 6;
+    b = 2;
+    c = a / b;
+    x = a / b;
+    x = a / (float)b;
+    //Операція % - взята від остачі ділення
+    c = a % b;
+
+}
